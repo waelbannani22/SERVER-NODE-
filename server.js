95ccec8db1fc9f3aa468cf62d9c9e7d678c29c71
waelbannani22 <79256781+waelbannani22@users.noreply.github.com>
2022-03-18 19:40:16 +0100

    proc

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ db.once("open", function () {
 });
 app.use(Router);
 app.use(RouterRecruiter);
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("server is running on 3000")
 });
     
